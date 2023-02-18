419be26d8e32a3f9a703008d3102c2c37a7cff81
Johannes Walter <hannibali@gmx.de>
2023-02-18 10:40:33 +0100

    sorted settings

--- settings.lua
+++ settings.lua
@@ -4,18 +4,22 @@ data:extend(
         name = "qts_language_setting",
         type = "string-setting",
         setting_type = "runtime-global",
-        allowed_values = {"qts_host", "qts_internal", "qts_player"},
-        default_value = "qts_internal"
+        allowed_values = {"qts_internal"},
+        --allowed_values = {"qts_host", "qts_internal", "qts_player"},
+        default_value = "qts_internal",
+        order = "aaa"
     },{
         name = "qts_enable_refueling_setting",
         type = "bool-setting",
         setting_type = "runtime-global",
-        default_value = true
+        default_value = true,
+        order = "caa"
     },{
         name = "qts_show_not_found_setting",
         type = "bool-setting",
         setting_type = "runtime-global",
-        default_value = true
+        default_value = true,
+        order = "cba"
     },
 --    {
 --        name = "qts_allow_longer_trains_setting",
@@ -32,11 +36,13 @@ data:extend(
         name = "qts_refuel_refresh_time_setting",
         type = "int-setting",
         setting_type = "runtime-global",
-        default_value = 120
+        default_value = 120,
+        order = "cxa"
     },{
         name = "qts_refuel_refresh_time_offset_setting",
         type = "int-setting",
         setting_type = "runtime-global",
-        default_value = 113
+        default_value = 113,
+        order = "cxb"
     }
 })
